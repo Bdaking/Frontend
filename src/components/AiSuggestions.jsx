dd3310ad122e0b3d1dc53fea136f57a5ccecb848
Bda <bdab709@gmail.com>
2026-05-25 22:39:54 +0530

    fix: align suggestions layout with fallback user extraction ladder

--- src/components/AiSuggestions.jsx
+++ src/components/AiSuggestions.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect } from "react";
-import axiosInstance from "../../utils/axiosInstance";
+import axiosInstance from "../utils/axiosInstance";
 import { API_PATHS } from "../utils/apiPath";
 
 const AiSuggestions = ({ data }) => {
